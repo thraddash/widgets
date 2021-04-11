03e5f3d5929cd125d8f16811dd6aae288b58df81
thraddash <theartist@gmail.com>
2021-04-11 15:42:38 -0700

    add onClick on item index

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,18 +1,22 @@
 import React from 'react';
 
 const Accordion = ({ items }) => {
-    const renderedItems = items.map(item => {
-        return <React.Fragment key={item.title}>
-            <div className="title active">
-                <i className="dropdown icon"></i>
-                {item.title}
-            </div>
-            <div className="content active">
-                <p>{item.content}</p>
-            </div>
-        </React.Fragment>
+    const renderedItems = items.map((item, index) => {
+        return (
+            <React.Fragment key={item.title}>
+                <div 
+                    className="title active"
+                    onClick={() => console.log('Title Clicked', index)}
+                >
+                    <i className="dropdown icon"></i>
+                    {item.title}
+                </div>
+                <div className="content active">
+                    <p>{item.content}</p>
+                </div>
+            </React.Fragment>
+        );
     });
-
     return <div className="ui styled accordion">{renderedItems}</div>
 };
 
