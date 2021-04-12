bfe1973f3b1e918d9d978688e06662ca42b651fe
thraddash <theartist@gmail.com>
2021-04-12 16:36:09 -0700

    useState hook system

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
+// update state in React without setState, use Hook System 
+// { useState } allows state variables in a functional component
 
 const Accordion = ({ items }) => {
+    // destructure two values out of useState
+    // activeIndex - reference to useState
+    const [activeIndex, setActiveIndex] = useState(null);
+
     const onTitleClick = (index) => {
-        console.log('Title clicked', index);
+        // updates state to current value, re-render
+        setActiveIndex(index);
     };
 
     const renderedItems = items.map((item, index) => {
@@ -21,7 +28,10 @@ const Accordion = ({ items }) => {
             </React.Fragment>
         );
     });
-    return <div className="ui styled accordion">{renderedItems}</div>
+    return <div className="ui styled accordion">
+        {renderedItems}
+        <h1>{activeIndex}</h1>
+    </div>;
 };
 
 export default Accordion;
