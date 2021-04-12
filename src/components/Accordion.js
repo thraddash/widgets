574d21e7dc65e3416fb5076ac9479c16dde16baa
thraddash <theartist@gmail.com>
2021-04-11 17:17:20 -0700

    add helper method onTitleClick to funcion component

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
 const Accordion = ({ items }) => {
+    const onTitleClick = (index) => {
+        console.log('Title clicked', index);
+    };
+
     const renderedItems = items.map((item, index) => {
         return (
             <React.Fragment key={item.title}>
                 <div 
                     className="title active"
-                    onClick={() => console.log('Title Clicked', index)}
+                    onClick={() => onTitleClick(index)}
                 >
                     <i className="dropdown icon"></i>
                     {item.title}
