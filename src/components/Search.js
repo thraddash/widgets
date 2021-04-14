ad6c3b158fd712c8c15b6148d121282aa7340533
thraddash <theartist@gmail.com>
2021-04-13 17:08:31 -0700

    add a href link

--- src/components/Search.js
+++ src/components/Search.js
@@ -5,8 +5,6 @@ const Search = () => {
     const [term, setTerm] = useState('');
     const [results, setResults] = useState([]);
 
-    console.log(results);
-
     useEffect(() => {
         const search = async () => {
             const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
@@ -27,6 +25,27 @@ const Search = () => {
         }
     }, [term]);
 
+    const renderedResults = results.map((result) => {
+        return (
+            <div key={result.pageid} className="item">
+                <div className="right floated content">
+                    <a 
+                        className="ui button"
+                        href={`https://en.wikipedia.org?curid=${result.pageid}`}
+                    >
+                        Go
+                    </a>
+                </div>
+                <div className="content">
+                    <div className="header">
+                        {result.title}    
+                    </div>
+                    <span dangerouslySetInnerHTML={{ __html: result.snippet}}></span>
+                </div>
+            </div>
+        )
+    });
+
     return (
         <div>
             <div className="ui form">
@@ -38,6 +57,9 @@ const Search = () => {
                         className="input" 
                     />
                 </div>
+            </div>
+            <div className="ui celled list">
+                {renderedResults}
             </div>
         </div>
     );
