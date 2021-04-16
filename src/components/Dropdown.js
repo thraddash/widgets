cefb158f373ff960ea402899fd406789910459dc
thraddash <theartist@gmail.com>
2021-04-16 13:43:54 -0700

    add new state open, setOpen to hide open/close list item, ternary expression on semantic ui

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
+    //hiding and showing option list
+    const [open, setOpen] = useState(false);
+
     const renderedOptions = options.map((option) => {
         if(option.value === selected.value) {
             return null;
         }
-
 
         return (
             <div
@@ -21,10 +23,10 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
         <div className="ui form">
             <div className="field">
                 <label className="label">Select a Color</label>
-                <div className="ui selection dropdown visible active">
+                <div onClick={() => setOpen(!open)} className={`ui selection dropdown ${open ? 'visible active' : ''}`}>
                     <i className="dropdown icon"></i>
                     <div className="text">{selected.label}</div>
-                    <div className="menu visible transition">
+                    <div className={`menu ${open ? 'visible transition' : ''}`}>
                         {renderedOptions}
                     </div>
                 </div>
