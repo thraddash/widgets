4c5fd26b15c24613b883239cf37449e440ff3f80
thraddash <theartist@gmail.com>
2021-04-19 23:35:44 -0700

    display translated text, use helper function wrap axios to use await

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -7,17 +7,31 @@ const GOOGLE_API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
 // receive language and text prop
 // Anytime language or text changes, useEffect hook will be run
 const Convert = ({ language, text }) => {
+    //store translated text, default value empty string
+    const [translated, setTranslated] = useState('');
+
+    // helper function to use await axios
     useEffect(() => {
-        axios.post('https://translation.googleapis.com/language/translate/v2', {}, {
-            params: {
-                q: text,
-                target: language.value,
-                key: GOOGLE_API_KEY
-            }
-        });
+        const doTranslation = async () => {
+            const { data } = await axios.post('https://translation.googleapis.com/language/translate/v2', {}, {
+                params: {
+                    q: text,
+                    target: language.value,
+                    key: GOOGLE_API_KEY
+                }
+            });
+
+            setTranslated(data.data.translations[0].translatedText);
+        };
+
+        doTranslation();
     }, [language, text]);
 
-    return <div />;
+    return (
+      <div>
+          <h1 className="ui header">{translated}</h1>
+      </div>  
+    );
 };
 
 export default Convert;
