7079ddcea68a4ed11ab5b2e0e1f9ecd6dc9effd9
thraddash <theartist@gmail.com>
2021-04-19 23:46:17 -0700

    debounced translation

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -10,12 +10,25 @@ const Convert = ({ language, text }) => {
     //store translated text, default value empty string
     const [translated, setTranslated] = useState('');
 
-    // helper function to use await axios
+    // set timer to update debounchedText 500ms, return cleanup function to cancel timer
+    const [debouncedText, setDebouncedText] = useState(text);
+
     useEffect(() => {
+        const timerId = setTimeout(() => {
+            setDebouncedText(text);
+        }, 500);
+
+        return () => {
+            clearTimeout(timerId);
+        }
+    }, [text]);
+
+    useEffect(() => {
+        // helper function to use await axios
         const doTranslation = async () => {
             const { data } = await axios.post('https://translation.googleapis.com/language/translate/v2', {}, {
                 params: {
-                    q: text,
+                    q: debouncedText,
                     target: language.value,
                     key: GOOGLE_API_KEY
                 }
@@ -25,7 +38,7 @@ const Convert = ({ language, text }) => {
         };
 
         doTranslation();
-    }, [language, text]);
+    }, [language, debouncedText]);
 
     return (
       <div>
